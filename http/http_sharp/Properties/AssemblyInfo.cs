684359d7406ad3244d696e1344510b5967fca7e6
Art Navsegda <artnavsegda@gmail.com>
2020-02-03 13:54:09 +0300

    okesy

---
+++ http/http_sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("http_sharp")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("http_sharp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
